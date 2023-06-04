01aa3a4b9b7625d597c2eded551064b09b67377b
13247673079 <swtaocyt@gmail.com>
2023-06-04 16:29:06 +0800

    202364

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,10 @@
 import { createApp } from 'vue'
 import './app.scss'
-import {Grid, GridItem,Pagination,Sticky, Tabs, TabPane   } from '@nutui/nutui-taro'
+import {Grid, GridItem,Pagination,Sticky, Tabs, TabPane,Popup ,Collapse, CollapseItem,Notify ,List  ,Comment,Button   } from '@nutui/nutui-taro'
 
 const App = createApp({
   onShow (options) {},
   // 入口组件不需要实现 render 方法，即使实现了也会被 taro 所覆盖
 })
-App.use(Grid).use(GridItem).use(Pagination).use(Sticky).use(Tabs).use(TabPane)
+App.use(Grid).use(Button.).use(GridItem).use(Pagination).use(Sticky).use(Tabs).use(TabPane).use(Popup).use(Collapse).use(List).use(CollapseItem).use(Notify).use(Comment)
 export default App
